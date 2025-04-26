02191f75c6379fe32852cdeb315a77a6b2d082ee
Ashleylq <ironmanashley@gmail.com>
2025-04-26 05:55:07 +0000

    Fixed create project

--- src/projects.js
+++ src/projects.js
@@ -1,21 +1,18 @@
 let projects = [];
  
-const createProject = function(name){
-    const name = name;
-    const todoList = [];
-    const addTodo = function(title, priority, description, dueDate){
-        const title = title;
-        const priority = priority;
-        const description = description;
-        const dueDate = dueDate;
+class CreateProject {
+    constructor(name){
+        this.name = name;
+    }
+    todoList = [];
+    #addTodo = function(title, priority, description, dueDate){
         return {title, priority, description, dueDate};
     }
-    const addTodoToArray = function(title, priority, description, dueDate){
-        todoList.push(addTodo(title, priority, description, dueDate));
+    addTodoToArray = function(title, priority, description, dueDate){
+        this.todoList.push(this.#addTodo(title, priority, description, dueDate));
     }
-    return {name, todoList, addTodoToArray}
 }
 
-const addProjectToArray = function(name){
-   projects.push(createProject(name));
+const addProjectToArray = (name) => {
+   projects.push(new CreateProject(name));
 }
